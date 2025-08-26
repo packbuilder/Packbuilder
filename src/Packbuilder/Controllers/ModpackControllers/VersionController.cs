649b80a9ed9bc12dfc6971822dc3523a80f1eded
Goob-tech <solizg02@gmail.com>
2025-08-26 13:51:47 -0500

    Created get version mod test and it works, used dto in controller to avoid circular reference error

--- src/Packbuilder/Controllers/ModpackControllers/VersionController.cs
+++ src/Packbuilder/Controllers/ModpackControllers/VersionController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Packbuilder.Dto.ModpackDtos;
+using Packbuilder.Interfaces;
 using Packbuilder.Models;
 using Packbuilder.Services;
 
@@ -7,13 +9,22 @@ namespace Packbuilder.Controllers.ModpackControllers
 {
     [ApiController]
     [Route("{username}/modpacks/{slug}/versions")]
-    public class VersionController(PackbuilderContext context, SessionService sessionService) : ControllerBase
+    public class VersionController(PackbuilderContext context, ISessionService sessionService) : ControllerBase
     {
         [HttpGet("{versionId}")]
         [EndpointName("GetVersion")]
         public async Task<ActionResult<ModpackVersion>> GetVersion([FromRoute] int versionId)
         {
-            return Ok(await context.Versions.Include(v => v.VersionMods).SingleOrDefaultAsync(v => v.Id == versionId));
+            ModpackVersion? version = await context.Versions.Include(v => v.VersionMods).SingleOrDefaultAsync(v => v.Id == versionId);
+
+            if (version == null)
+            {
+                return NotFound();
+            }
+
+            ModpackVersionDto versionDto = new(version, version.ModpackId, version.Iteration);
+
+            return Ok(versionDto);
         }
 
         [HttpPost]
@@ -39,6 +50,11 @@ namespace Packbuilder.Controllers.ModpackControllers
                     mods.Add(mod);
                 }
             });
+
+            if (mods.Count == 0)
+            {
+                return BadRequest();
+            }
 
             ModpackVersion newVersion = currentModpack.CreateVersion(mods);
 
