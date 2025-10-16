1ae20e9b9674b30f2989c324e6da397b5e2a1fae
Goob-tech <solizg02@gmail.com>
2025-10-16 15:43:55 -0500

    Got rid of image from jwt token lmaoooo

--- src/Packbuilder/Services/SessionService.cs
+++ src/Packbuilder/Services/SessionService.cs
@@ -52,7 +52,7 @@ namespace Packbuilder.Services
             {
                 new Claim(ClaimTypes.Name, user.Name),
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim("Picture", user.Avatar),
+                new Claim(ClaimTypes.Email, user.Email),
                 new Claim("UpdatedAt", user.UpdatedAt.ToString("o")),
                 new Claim("CreatedAt", user.CreatedAt.ToString("o"))
             };
